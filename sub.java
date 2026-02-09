437da5d67d7a5086f00d0e108675e55c5540d8e4
raju15022004 <raju032004@gmail.com>
2026-02-10 01:00:39 +0530

    sub.java

--- sub.java
+++ sub.java
@@ -5,6 +5,21 @@ public class sub{
 
   public static void main(String[] args) {
       Scanner sc=new Scanner(System.in);
-      System.out.print("Englisg");
+      System.out.print("Englisg ");
+      int eng=sc.nextInt();
+      System.out.print("Computer = ");
+      int com=sc.nextInt();
+      int total=eng+com;
+      if(total>=90 && total<100){
+        System.out.print("your grade is A");
+
+      }
+      else if(total>=80 && total<90){
+          System.out.print("your grade is B");
+      }
+      else{
+        System.out.print("you are fail");
+      }
+
   }
 }
