1daa1531796a16a7aa2013bebf4ae24d7ff7ada7
raju15022004 <raju032004@gmail.com>
2026-02-09 19:02:11 +0530

    second.java

--- second.java
+++ second.java
@@ -1,7 +1,7 @@
 public class second{
   public static void main(String[] args) {
       int num1=10;
-      int num2=3;
+      int num2=4;
       int remainder=num1%num2;
       System.out.println(remainder);
   }
