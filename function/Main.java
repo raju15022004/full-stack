0e169f1e78343e1558463afc8f749c312c8ecfd4
raju15022004 <raju032004@gmail.com>
2026-02-15 22:48:21 +0530

    Main.java

---
+++ function/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+
+  String name;
+  int age;
+
+  public static void main(String[] args) {
+      Main obj=new Main();
+      obj.name="raju";
+      obj.age=23;
+
+      System.out.println("name ="+obj.name);
+      System.out.print("age ="+obj.age);
+
+  }
+}
