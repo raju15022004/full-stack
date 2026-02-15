d6ff28ecdfdb1b8312c8184f88e3860b1cedd72a
raju15022004 <raju032004@gmail.com>
2026-02-15 22:27:30 +0530

    even.java

---
+++ function/even.java
@@ -0,0 +1,14 @@
+public class even{
+
+  static boolean  ifeven(int a){
+    if(a%2==0){
+      return true;
+    }else{
+      return false;
+    }
+
+  }
+  public static void main(String[] args) {
+      System.out.println(ifeven(7));
+  }
+}
