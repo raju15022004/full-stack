38f5f21b55b02cd104b27f13b4ba3553b45f2a62
raju15022004 <raju032004@gmail.com>
2026-02-19 11:22:51 +0530

    max.java

---
+++ Array/max.java
@@ -0,0 +1,16 @@
+public class max{
+
+  public static void main(String[] args) {
+
+      int arr[]={10,5,30,40,50};
+
+      int max=arr[0];
+      for(int i=0;i<arr.length;i++){
+        if(max<arr[i]){
+          max=arr[i];
+
+        }
+      }
+      System.out.println("max element"+max);
+  }
+}
