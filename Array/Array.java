fec487bfb1e707fe70b3c29f50f73b66ce49b113
raju15022004 <raju032004@gmail.com>
2026-02-19 11:08:10 +0530

    Array.java

--- Array/Array.java
+++ Array/Array.java
@@ -6,5 +6,17 @@ public class Array{
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     System.out.println("Raju");
+    int size=sc.nextInt();
+
+    int age[]=new int[size];
+
+    for(int i=0;i<size;i++){
+      age[i]=sc.nextInt();
+    }
+    System.out.println("Student's list");
+
+    for(int i:age){
+      System.out.println(i);
+    }
   }
 }
