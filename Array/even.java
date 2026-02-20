8065725718d25b2e267a94ae92c57425544cacba
raju15022004 <raju032004@gmail.com>
2026-02-20 10:56:45 +0530

    even.java

---
+++ Array/even.java
@@ -0,0 +1,18 @@
+public class even{
+
+  public static void main(String[] args) {
+      int number[]={1,2,3,4,5,6,7,8};
+      int even=0;
+      int odd=0;
+
+      for(int i=0;i<number.length;i++){
+        if(number[i]%2==0){
+          even++;
+        }else{
+          odd++;
+        }
+      }
+      System.out.println("even ="+even);
+      System.out.println("odd ="+odd);
+  }
+}
