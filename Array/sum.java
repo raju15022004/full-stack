e802acfbb5076b5e8d7fa9089266cb0aa2934606
raju15022004 <raju032004@gmail.com>
2026-02-20 10:47:40 +0530

    sum.java

---
+++ Array/sum.java
@@ -0,0 +1,15 @@
+public class sum{
+
+  public static void main(String[] args){
+
+    int number[]={10,5,25,30,40};
+
+      int sum=0;
+      for(int i=0;i<number.length;i++){
+        sum +=number[i];
+      }
+
+      System.out.println("sum ="+sum);
+
+  }
+}
