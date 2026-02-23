660c618cedc840b54f994063ba716113fffefde6
raju15022004 <raju032004@gmail.com>
2026-02-23 22:58:05 +0530

    buble.java

---
+++ Array/bubble.java
@@ -0,0 +1,26 @@
+public class bubble {
+
+  public static void main(String[] args) {
+      int arr[]={10,2,4,3,7,6,8};
+      int n=arr.length;
+      System.out.println("Before Sorting");
+      for(int i=0;i<n;i++){
+        System.out.print(arr[i]+ " ");
+      }
+        for(int i=0;i<n-1;i++){
+          for(int j=0;j<n-1-i;i++){
+            if(arr[j]>arr[j+1]){
+              int temp=arr[j];
+              arr[j]=arr[j+1];
+              arr[j+1]=temp;
+            }
+          }
+        }
+
+        System.out.println("After Sorting");
+       for(int i=0;i<n;i++){
+        System.out.print(arr[i]+" ");
+       }
+  }
+
+}
