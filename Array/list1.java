3096e36cd1b5cdd62604e373739fe4be796ac6fa
raju15022004 <raju032004@gmail.com>
2026-02-24 00:20:56 +0530

    list1.java

---
+++ Array/list1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+
+public class list1 {
+
+  public static void main(String[] args) {
+     ArrayList<String> list=new ArrayList<String>();
+      list.add("tony");
+      list.add("Stark");
+      list.add(1, "iron");
+
+        System.out.println(list);
+
+        // Set value at index 0
+        list.set(0, "iron man");
+
+        System.out.println(list);
+  }
+
+}
+
+
+
