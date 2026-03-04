776cdecc4e0b37e717818710da70a0ffe229cbd8
raju15022004 <raju032004@gmail.com>
2026-03-04 17:08:11 +0530

    list1.java

--- Array/list1.java
+++ Array/list1.java
@@ -6,7 +6,7 @@ public class list1 {
   public static void main(String[] args) {
      ArrayList<String> list=new ArrayList<String>();
       list.add("raju");
-      list.add("Sonam");
+      list.add("sonu");
       list.add(1, "Sonam");
 
         System.out.println(list);
