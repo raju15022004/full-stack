ea2e4eacb8f9d97d68d73b2d1617c638ce66e723
raju15022004 <raju032004@gmail.com>
2026-04-02 08:48:44 +0530

    bubble.java

--- chapter01/link.java
+++ chapter01/link.java
@@ -1,4 +1,42 @@
 public class link {
-  
+
+  class Node{
+    String data;
+    Node next;
+
+    Node(String value){
+      data=value;
+      next=null;
+    }
+  }
+
+  Node head;
+
+  public void addFirst(String data){
+    Node newNode=new Node(data);
+
+    if(head==null){
+      head=newNode;
+      return;
+    }
+    newNode.next=head;
+    head=newNode;
+  }
+
+  public void show(){
+
+    Node temp=head;
+    while(temp!=null){
+      System.out.print(temp.data + " -> ");
+      temp=temp.next;
+    }
+  }
+
+  public static void main(String[] args) {
+    link list=new link();
+    list.addFirst(data:"2");
+    list.addFirst(data:"1");
+    list.show();
+  }
 
 }
