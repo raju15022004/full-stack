c6c1f719fd9529779720b3ee6526838e46b7b98c
raju15022004 <raju032004@gmail.com>
2026-04-04 14:06:49 +0530

    frame.java

---
+++ chapter01/frame.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JFrame;
+
+public class frame {
+
+
+  public static void main(String[] args) {
+      JFrame frame=new JFrame("main Frame");
+      frame.setSize(400,400);
+      frame.setLayout(null);
+      frame.setVisible(true);
+  }
+
+}
--- chapter01/link.java
+++ chapter01/link.java
@@ -32,10 +32,25 @@ public class link {
     }
   }
 
+  public void addLast(String data){
+    Node newNode=new Node(data);
+
+    if(head==null){
+      head=newNode;
+      return;
+    }
+
+    Node temp=head;
+    while(temp.next!=null){
+      temp=temp.next;
+    }
+    temp.next=newNode;
+  }
   public static void main(String[] args) {
     link list=new link();
     list.addFirst("2");
-        list.addFirst("1");
+    list.addFirst("1");
+    list.addLast(data: "3");
     list.show();
   }
 
