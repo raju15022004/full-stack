95ed954a59d68783da9cac4bfd49d91857298d96
raju15022004 <raju032004@gmail.com>
2026-04-04 14:37:57 +0530

    frame.java

--- chapter01/frame.java
+++ chapter01/frame.java
@@ -1,5 +1,6 @@
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class frame {
 
@@ -9,6 +10,13 @@ public class frame {
       frame.setSize(400,400);
       frame.setLayout(null);
       frame.setVisible(true);
+      // JButton b1=new JButton("Click me");
+      // b1.setBounds(120,100,100,50);
+      // frame.add(b1);
+
+      JTextField t1=new JTextField("user name");
+      t1.setBounds(100,100,100,30);
+      frame.add(t1);
   }
 
 }
