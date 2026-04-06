37dad52155c1d4e2156e405258f1b9f45b3c3f80
raju15022004 <raju032004@gmail.com>
2026-04-06 20:35:06 +0530

    frame.java

--- chapter01/frame.java
+++ chapter01/frame.java
@@ -1,6 +1,6 @@
 
 import javax.swing.JFrame;
-import javax.swing.JTextField;
+import javax.swing.JTable;
 
 public class frame {
 
@@ -14,9 +14,17 @@ public class frame {
       // b1.setBounds(120,100,100,50);
       // frame.add(b1);
 
-      JTextField t1=new JTextField("user name");
-      t1.setBounds(100,100,100,30);
-      frame.add(t1);
+      // JTextField t1=new JTextField("user name");
+      // t1.setBounds(100,100,100,30);
+      // frame.add(t1);
+
+      String columns[]={"ID","Name","Salary"};
+      String data[][]={{"100","raju","50000"},
+      {"102","sani","60000"},{"103","Qader","70000"}};
+
+      JTable tb1=new JTable(data,columns);
+      tb1.setBounds(30,50,300,4000);
+      frame.add(tb1);
   }
 
 }
