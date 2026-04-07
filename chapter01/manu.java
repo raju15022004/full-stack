0782085a75520a0f14a4a5b07770078a30e8794a
raju15022004 <raju032004@gmail.com>
2026-04-07 20:49:52 +0530

    manu.java

---
+++ chapter01/manu.java
@@ -0,0 +1,36 @@
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class manu {
+
+  public static void main(String[] args) {
+      JFrame f=new JFrame("main");
+      f.setSize(400,400);
+      f.setLayout(null);
+      f.setVisible(true);
+      JMenuBar mb=new JMenuBar();
+      JMenu menu,subMenu;
+      JMenuItem i1,i2,i3,i4,i5;
+      menu=new JMenu("Menu");
+      subMenu=new JMenu("Sub Menu");
+      i1=new JMenuItem("1");
+      i2=new JMenuItem("2");
+      i3=new JMenuItem("3");
+      i4=new JMenuItem("4");
+      i5=new JMenuItem("5");
+
+      menu.add(i1);
+      menu.add(i2);
+      menu.add(i3);
+      subMenu.add(i4);
+      subMenu.add(i5);
+      menu.add(subMenu);
+      mb.add(menu);
+      f.setJMenuBar(mb);
+
+  }
+
+}
