c7065c0e8d4f3b4d6e2ab3c5cbe13f626f68ad49
raju15022004 <raju032004@gmail.com>
2026-04-08 20:19:35 +0530

    file.java

--- chapter01/file.java
+++ chapter01/file.java
@@ -4,7 +4,7 @@ import javax.swing.JFileChooser;
 public class file {
 
   public static void main(String[] args) {
-      JFileChooser jf=new JFileChooser("c:");
+      JFileChooser jf=new JFileChooser("d:");
       jf.showSaveDialog(null);
   }
 
