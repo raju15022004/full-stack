d89dc1085824bc556ecf65614aa1c02d4e40b07b
raju15022004 <raju032004@gmail.com>
2026-04-11 20:05:43 +0530

    file.java

---
+++ Backend/file.java
@@ -0,0 +1,16 @@
+
+import java.io.File;
+import java.util.Scanner;
+
+public class file {
+  public static void main(String[] args) {
+
+    try{
+      File obj=new File("myfile.txt");
+      Scanner sc=new Scanner(obj);
+  }catch(Exception e){
+    System.out.println("File not found");
+  }
+  }
+
+}
--- Backend/first.java
+++ Backend/first.java
@@ -4,8 +4,14 @@ public class first {
 
   public static void main(String[] args) {
       int i=8;
-      int j=2;
+      int j=0;
+
+      try {
       System.out.println(i/j);
+      } catch (Exception e) {
+        System.out.println("second value can not a zero"+e);
+      }
+      System.out.println("raju");
   }
 
 }
