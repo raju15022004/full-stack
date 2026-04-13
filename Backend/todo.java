f6da9ee86473b2c2a28dba12cfd9b23c1890099d
raju15022004 <raju032004@gmail.com>
2026-04-13 11:05:30 +0530

    child.java

---
+++ Backend/todo.java
@@ -0,0 +1,19 @@
+
+import javax.swing.DefaultListModel;
+
+public class todo extends javax.swing.JFrame {
+  private DefaultListModel<String>
+
+  public todo(){
+    initComponents();
+    1m=new DefaultListModel<>();
+    1l.setModel(1m);
+  }
+
+  private void jButton2ActionPerformed(java.awt.event.ActionEvent){
+
+  }
+
+
+
+}
